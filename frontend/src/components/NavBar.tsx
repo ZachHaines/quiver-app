f1d47a62ffa1d9da287f85b544fa8434c14728b2
Zachary Haines <hainesz47@gmail.com>
2023-03-03 01:51:37 -0500

    styled navbar for member view

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,33 +1,99 @@
-import { Link } from 'react-router-dom'
-import { AppBar, Toolbar, IconButton, Typography } from '@mui/material'
-import unitInsignia from '../images/unit_insignia_small.png'
+import { Link } from 'react-router-dom';
+import styled from 'styled-components'
+import unitInsignia from '../images/unit_insignia_small.png';
+/* MUI Imports */
+import { AppBar, Toolbar, IconButton, Typography } from '@mui/material';
+import Box from '@mui/material/Box'
+import EventIcon from '@mui/icons-material/Event';
+import MessageIcon from '@mui/icons-material/Message';
+import PeopleIcon from '@mui/icons-material/People';
+import PollIcon from '@mui/icons-material/Poll';
+import TravelExploreIcon from '@mui/icons-material/TravelExplore';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
+
+
 
 const NavBar = () => {
   return (
-    <AppBar position='static'>
-      <Toolbar>
+    <AppBar position='static' style={{ background: '#024407' }}>
         
-          <Link to='/'>
-            <IconButton size='small' edge='start' color='inherit' aria-label='logo'>
-              <img src={unitInsignia} style={{height:"40px", width:'auto'}} className="icon-logo" alt="SF Logo" />
-            </IconButton>
-          </Link>
-          <Typography variant='h6' component='div'>
-            <Link to="/">Home</Link> |
-            <Link to="/login">Login</Link> |
-            <Link to="/contact">Contact Us</Link> |
-            <Link to="/member">Members</Link> | 
-            <Link to="/network">My Network</Link> | 
-            <Link to="/messaging">Messaging</Link> | 
-            <Link to="/events">Events</Link> | 
-            <Link to="/surveys">Surveys</Link> | 
-            <Link to="/resources">Resources</Link> | 
-          </Typography>
-      </Toolbar>
+          <Toolbar>
+            <Link to='/'>
+              <IconButton size='small' edge='start' color='inherit' aria-label='logo'>
+                <img src={unitInsignia} style={{height:"40px", width:'auto'}} alt="SF Logo" />
+              </IconButton>
+              </Link>
+            <Box display='flex' flexGrow={1}>
+              <Link to='/' style={{ color: 'inherit', textDecoration: 'none'}}>
+                <StyledNavTitle>QUIVER</StyledNavTitle>
+              </Link>
+            </Box>
+            <Typography variant='h5' component='div' style={{padding: '0 32px', display: 'inline-flex'}}>
+              <StyledLink to="/network">
+                <PeopleIcon/>
+                My Network
+              </StyledLink>
+              <StyledLink to="/messaging">
+                <MessageIcon />
+                Messaging
+              </StyledLink>
+              <StyledLink to="/events">
+                <EventIcon />
+                Events</StyledLink>
+              <StyledLink to="/surveys">
+                <PollIcon />
+                Surveys
+              </StyledLink>
+              <StyledLink to="/resources">
+                <TravelExploreIcon />
+                Resources
+              </StyledLink>    
+            </Typography>
+      
+              <Typography variant='h5' component='div' style={{padding: '0 8px 0 32px', display: 'inline-flex'}}>
+              <StyledLink to="/profile">
+                <AccountCircleIcon  />
+                Me
+              </StyledLink> 
+            </Typography>
+       
+          </Toolbar>
+      
     </AppBar>
-
   )
 
 }
 
 export default NavBar;
+
+
+const StyledNavTitle = styled.h1`
+  color: white;
+  padding: 0 32px 0 8px;
+  text-shadow: 2px 2px 5px black;
+  &:active {
+    text-shadow: none;
+  }
+  &:hover {
+    color: #ECD592;
+  }
+  `;
+  
+  const StyledLink = styled(Link)`
+  color: inherit;
+  font-weight: bold;
+  justify-content: center;
+  text-decoration: none;
+  text-shadow: 2px 2px 5px black;
+  padding: 0 24px;
+  display: flex;
+  flex-direction: column;
+  &:active {
+    text-shadow: none;
+    font-weight: normal;
+  }
+  &:hover {
+    color: #ECD592;
+  }
+`;
